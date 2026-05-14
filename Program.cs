9edabd9ce46d308d33decb5d1417c5b5c8aa4011
is1omzhon <islomjonsharipov003@gmail.com>
2026-05-14 23:19:54 +0300

    Added readme

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
 ﻿var opel = new Car();
+
 opel.Brand = "Opel";
 opel.Model = "Sidan";
 opel.Year = 2010;
-opel.FuelLevel = 20;
+opel.FuelLevel = 5;
+
 opel.ShowInfo();
 opel.Drive(100);
+opel.Refuel(15);
+
+Console.WriteLine();
+
+var lada = new Car()
+{
+    Brand = "Lada",
+    Model = "Largus",
+    Year = 2020,
+    FuelLevel = 0
+};
+
+lada.ShowInfo();
+lada.Refuel(105);
+
+
+
 
 class Car
 {
@@ -15,33 +34,47 @@ class Car
 
     public void ShowInfo()
     {
-        Console.WriteLine($"Brend: {Brand}, Model: {Model}, Year: {Year}, FuelLevel: {FuelLevel}%");
+        Console.WriteLine($"Brend: {Brand}, Model: {Model}, Year: {Year}, FuelLevel: {FuelLevel}% ");
     }
 
     public void Drive(int distance)
     {
         Console.Write($"Berilgan masofa: {distance}km \n");
+        
+        int countFuel = FuelLevel;
+        int countDistance = distance;
 
-        for (int i = 0; i < FuelLevel; i++)
+        for (int i = 0; i <= FuelLevel; i++)
         {
-            
-            if(FuelLevel > 0)
+            if (countFuel <= 0)
             {
-                FuelLevel--;
-                distance-=10;
+                Console.WriteLine($"Not enough fuel. Qolgan masofa: {countDistance}km ");
+                break;
+            }
+
+            else if(countFuel > 0)
+            {
+                countFuel--;
+                countDistance -= 10;
                 
-                if(distance == 0)
+                if(countDistance <= 0)
                 {
-                    Console.WriteLine($"Siz masofani bosib utdingiz.Sizda {FuelLevel}% yoqilg'i qoldi.");
+                    Console.WriteLine($"Siz {distance}km masofani bosib utdingiz.Sizda {countFuel}% yoqilg'i qoldi.");
                     break;
                 }
             }
-            else
-            {
-                Console.WriteLine("Not enough fuel");
-                break;
-            }
         }
+    }
 
+    public void Refuel(int amount)
+    {
+        int sumFuel = FuelLevel + amount;
+
+        if(sumFuel > 100)
+        {
+            sumFuel = 100;
+        }
+        Console.WriteLine($"Yoqilgi quyildi. Hozirgi holat: {sumFuel}%");
     }
 }
+
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# OOP
+# 🚗 Car Management System
+
+C# da yozilgan oddiy avtomobil boshqaruv tizimi. OOP tamoyillarini amalda qo‘llash uchun yaratilgan.
+
+---
+
+## 📋 Loyiha haqida
+
+Ushbu loyiha quyidagi mavzularni o‘z ichiga oladi:
+- class  
+- object
+- field 
+- method
+- namespace 
+- using
+
+---
+
+## 🚀 Funksiyalar
+
+| Metod | Vazifasi |
+|-------|----------|
+| `ShowInfo()` | Mashina haqida to‘liq ma'lumot chiqaradi |
+| `Drive(distance)` | Mashinani yurgizadi (10 km = 1% yoqilg‘i) |
+| `Refuel(amount)` | Yoqilg‘i quyadi (100% dan oshmaydi) |
