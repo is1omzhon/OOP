6e9d585e79fedc24fa3dc0c19e2764332cc248b3
is1omzhon <islomjonsharipov003@gmail.com>
2026-05-14 20:06:46 +0300

    Added class Car

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var opel = new Car();
+opel.Brand = "Opel";
+opel.Model = "Sidan";
+opel.Year = 2010;
+opel.FuelLevel = 20;
+
+opel.ShowInfo();
+
+class Car
+{
+    public string Brand;
+    public string Model;
+    public int Year;
+    public int FuelLevel ;
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Brend: {Brand}, Model: {Model}, Year: {Year}, FuelLevel: {FuelLevel}%");
+    }
+}
