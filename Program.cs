670cc62aa2236ba20f593adbd9f30fe77b7cf6be
is1omzhon <islomjonsharipov003@gmail.com>
2026-05-14 20:29:27 +0300

    added new method

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ opel.Brand = "Opel";
 opel.Model = "Sidan";
 opel.Year = 2010;
 opel.FuelLevel = 20;
-
 opel.ShowInfo();
+opel.Drive(100);
 
 class Car
 {
@@ -16,5 +16,32 @@ class Car
     public void ShowInfo()
     {
         Console.WriteLine($"Brend: {Brand}, Model: {Model}, Year: {Year}, FuelLevel: {FuelLevel}%");
+    }
+
+    public void Drive(int distance)
+    {
+        Console.Write($"Berilgan masofa: {distance}km \n");
+
+        for (int i = 0; i < FuelLevel; i++)
+        {
+            
+            if(FuelLevel > 0)
+            {
+                FuelLevel--;
+                distance-=10;
+                
+                if(distance == 0)
+                {
+                    Console.WriteLine($"Siz masofani bosib utdingiz.Sizda {FuelLevel}% yoqilg'i qoldi.");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel");
+                break;
+            }
+        }
+
     }
 }
